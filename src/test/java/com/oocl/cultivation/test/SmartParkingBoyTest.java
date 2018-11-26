522e0fbe3212efe31f1b6232d11669b267e2fc08
J <jason19951020@gmail.com>
2018-11-26 23:29:05 +0800

    reorganized parkingboy testing method

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -2,12 +2,14 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.ParkingTicket;
 import com.oocl.cultivation.SmartParkingBoy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class SmartParkingBoyTest {
+    /*
     //Story 4
     @Test
     void test_smart_park_boy_on_select_lot(){
@@ -17,6 +19,19 @@ public class SmartParkingBoyTest {
         boy.manageLot(TargetLot);
         boy.park(new Car());
         assertSame(TargetLot, boy.getCurrentParkingLot());
+
+    }
+    */
+    //Story 4
+    @Test
+    void test_smart_boy_park_and_fetch_on_select_lot(){
+        ParkingLot nonTargetLot = new ParkingLot(1);
+        ParkingLot TargetLot = new ParkingLot(2);
+        SmartParkingBoy boy = new SmartParkingBoy(nonTargetLot);
+        boy.manageLot(TargetLot);
+        Car car = new Car();
+        ParkingTicket ticket = boy.park(car);
+        assertSame(car, boy.fetch(ticket));
 
     }
 }
--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -2,12 +2,14 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.ParkingTicket;
 import com.oocl.cultivation.SuperSmartParkingBoy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class SuperSmartParkingBoyTest {
+    /*
     //Story 5
     @Test
     void test_super_park_boy_on_select_lot(){
@@ -19,5 +21,19 @@ public class SuperSmartParkingBoyTest {
         boy.park(new Car());
         boy.park(new Car());
         assertSame(TargetLot, boy.getCurrentParkingLot());
+    }
+*/
+    //Story 5
+    @Test
+    void test_super_park_boy_park_and_fetch_on_select_lot(){
+        ParkingLot nonTargetLot = new ParkingLot(5);
+        ParkingLot TargetLot = new ParkingLot(10);
+        SuperSmartParkingBoy boy = new SuperSmartParkingBoy(nonTargetLot);
+        boy.manageLot(TargetLot);
+        boy.park(new Car());
+        boy.park(new Car());
+        Car car = new Car();
+        ParkingTicket ticket = boy.park(car);
+        assertSame(car, boy.fetch(ticket));
     }
 }
