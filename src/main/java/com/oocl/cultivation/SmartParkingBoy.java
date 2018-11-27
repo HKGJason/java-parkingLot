02f2173b2419e8f68ceac8a4b7d6167e217d8a0d
Jason Liu <jason.liu@oocl.com>
2018-11-27 10:23:21 +0800

    refactor: long method smartparkingboy park extract method

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,30 +1,35 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy {
+    private List<ParkingLot> lots = new ArrayList<>();
     public SmartParkingBoy(ParkingLot lot){
         super(lot);
     }
     @Override
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
-        List<ParkingLot> lots = super.getParkingLot();
-        ParkingLot lot = findMostEmpty(lots);
-        super.setCurrentParkingLot(lot);
+        ParkingLot lot = setParkingLot();
         if (lot!=null){
             lot.parkCar(car, ticket);
             super.setLastErrorMessage(null);
             return ticket;
-        }else{
-            return null;
         }
-
+        return null;
     }
 
-    private ParkingLot findMostEmpty(List<ParkingLot> lots) {
-        ParkingLot lot = lots.get(0);
-        for (ParkingLot p: lots){
+    private ParkingLot setParkingLot() {
+        this.lots = super.getParkingLot();
+        ParkingLot lot = findMostEmpty();
+        super.setCurrentParkingLot(lot);
+        return lot;
+    }
+
+    private ParkingLot findMostEmpty() {
+        ParkingLot lot = this.lots.get(0);
+        for (ParkingLot p: this.lots){
             if (p.getEmptyLotCount()>0 && p.getEmptyLotCount()>lot.getEmptyLotCount()){
                 lot = p;
             }
