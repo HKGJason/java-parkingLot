95325c892cf17bf73d0d3695ba0f09186e363c73
Jason Liu <jason.liu@oocl.com>
2018-11-27 12:35:34 +0800

    refactor: long method supersmartboy extract method

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,16 +1,17 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
+    private List<ParkingLot> lots = new ArrayList<>();
     public SuperSmartParkingBoy(ParkingLot lot){
         super(lot);
     }
     @Override
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
-        List<ParkingLot> lots = super.getParkingLot();
-        ParkingLot lot = findMostEmptyPercentage(lots);
+        ParkingLot lot = setParkingLot();
         super.setCurrentParkingLot(lot);
         if (lot!=null){
             lot.parkCar(car, ticket);
@@ -21,19 +22,28 @@ public class SuperSmartParkingBoy extends ParkingBoy {
         }
 
     }
+    private ParkingLot setParkingLot(){
+        this.lots = super.getParkingLot();
+        ParkingLot lot = findMostEmptyPercentage(this.lots);
+        super.setCurrentParkingLot(lot);
+        return findMostEmptyPercentage(this.lots);
+
+    }
 
     private ParkingLot findMostEmptyPercentage(List<ParkingLot> lots) {
         ParkingLot lot = lots.get(0);
-        float emptyRateCurrent;
-        float emptyRateBefore;
+        float emptyRateCurrent, emptyRateBefore;
         for (ParkingLot p: lots){
-            emptyRateBefore = (lot.getEmptyLotCount()*100.0f) / (lot.getCapacity()*100.0f);
-            emptyRateCurrent = (p.getEmptyLotCount()*100.0f) / (p.getCapacity()*100.0f);
+            emptyRateBefore = getEmptyPercentage(lot);
+            emptyRateCurrent = getEmptyPercentage(p);
             if (p.getEmptyLotCount()>0 && emptyRateCurrent>emptyRateBefore){
                 lot = p;
             }
         }
         return lot;
+    }
+    private float getEmptyPercentage(ParkingLot parkinglot){
+        return (parkinglot.getEmptyLotCount()*100.0f/parkinglot.getCapacity()*100.0f);
     }
 
 }
